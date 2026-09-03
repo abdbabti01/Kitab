584411b5383b89d4da6164ca7d01ffb019826512
babti <51147430+abdbabti01@users.noreply.github.com>
2026-09-03 10:50:08 -0400

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./generated.css";
+import "./curated.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><App /></React.StrictMode>);
